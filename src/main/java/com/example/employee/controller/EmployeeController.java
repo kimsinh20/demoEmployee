a5a583264d1ce118bef188521e01af43149ef6f4
kimsinh20 <88784882+kimsinh20@users.noreply.github.com>
2022-08-19 16:42:11 +0700

    First commit

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,74 @@
+package com.example.employee.controller;
+
+import com.example.employee.exception.ResourceNotFoundException;
+import com.example.employee.model.Employee;
+import com.example.employee.respository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+    @RestController
+    @RequestMapping("/api/v1/employees")
+    @CrossOrigin(origins = "http://localhost:3000")
+    public class EmployeeController {
+
+        @Autowired
+        private EmployeeRepository repository;
+
+        // get all employees
+        @GetMapping()
+        public List<Employee> getAllEmployees() {
+            return repository.findAll();
+        }
+
+        // create new employee
+        @PostMapping("")
+        public Employee createEmployee(@RequestBody Employee employee) {
+            return repository.save(employee);
+        }
+
+        // create new list employee
+        @PostMapping("/all")
+        public List<Employee> createEmployees(@RequestBody List<Employee> employee) {
+            return repository.saveAll(employee);
+        }
+
+        // get Employee by ID
+        @GetMapping("/{id}")
+        public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) throws ResourceNotFoundException {
+            Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+            return ResponseEntity.ok().body(employee);
+        }
+
+        // update Employee by id
+        @PutMapping("/{id}")
+        public ResponseEntity<?> updateEmployeeById(@PathVariable(value = "id") Long id, @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+            Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
+            employee.setFirstName(employeeDetails.getFirstName());
+            employee.setLastName(employeeDetails.getLastName());
+            employee.setEmail(employeeDetails.getEmail());
+
+            return ResponseEntity.ok().body(repository.save(employee));
+        }
+
+        // delete Employee by id
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Map<String, Boolean>> deleteEmployeeById(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
+            Employee employee = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id: " + id));
+
+            repository.delete(employee);
+            Map<String, Boolean> response = new HashMap<>();
+            response.put("deleted", Boolean.TRUE);
+            return ResponseEntity.ok(response);
+        }
+        //delete all
+    @DeleteMapping("/delete-all")
+    public void deleteAll() {
+            repository.deleteAll();
+    }
+
+}
---
+++ src/main/java/com/example/employee/model/Employee.java
@@ -0,0 +1,77 @@
+package com.example.employee.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public Employee() {
+    }
+
+    public Employee(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Employee(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
